00551cefc7028f0fff8f908fc90c63a2f7befbc1
Bardan-g <neupanebardang@gmail.com>
2024-11-19 14:38:34 +0545

    add initial screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,34 @@
-import { Text } from "react-native";
+import { StatusBar, Text, View } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
+import RecommendedWithIcon from "@/components/RecommendedWithIcon";
 
-function HomeScreen(){
-    return (
-        <Text>Home Screen</Text>
-    );
+function HomeScreen() {
+  const { top } = useSafeAreaInsets();
+  return (
+    <SafeAreaView className={`flex-1 bg-black p-3`}>
+      <View className="flex-1 p-5">
+        {/* title and search icon  */}
+        <View className=" mt-3 flex-row justify-between items-center">
+          <View className="m-2 gap-1">
+            <Text className="text-gray-400 text-xl ">Good Morning,</Text>
+            <Text className="text-2xl font-bold text-gray-300 ">Bardan G</Text>
+          </View>
+          <AntDesign name="search1" size={32} color="gray" />
+        </View>
+
+        {/* recommended for you */}
+        <View className="mt-4 ">
+          <RecommendedWithIcon text={"Recommended For You"} />
+        </View>
+      </View>
+
+      <StatusBar backgroundColor="#61dafb" />
+    </SafeAreaView>
+  );
 }
 
 export default HomeScreen;
